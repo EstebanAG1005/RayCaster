139a5b6840682d7b9fd1ebe4373550b59c4d96ec
EstebanAG1005 <60299338+EstebanAG1005@users.noreply.github.com>
2022-10-27 19:42:51 -0600

    Creacion de Lab 3

--- main.py
+++ main.py
@@ -1,12 +1,61 @@
-from cyglfw3 import *
-from OpenGL.GL import *
+import pygame
+import numpy as np
 
-glfw.Init()
+col_about_to_die = (200, 200, 225)
+col_alive = (255, 255, 215)
+col_background = (10, 10, 40)
+col_grid = (30, 30, 60)
 
-window = glfW.CreateWindow(800, 600, 'opengl')
+def update(surface, cells, cellsize):
+    nxt = np.zeros((cells.shape[0], cells.shape[1]))
 
-while not glfw.WindowShouldClose(window):
+    for r, c in np.ndindex(cells.shape):
+        num_alive = np.sum(cells[r-1:r+2, c-1:c+2]) - cells[r, c]
 
-    glfw.PollEvents()
+        if cells[r, c] == 1 and num_alive < 2 or num_alive > 3:
+            col = col_about_to_die
+        elif (cells[r, c] == 1 and 2 <= num_alive <= 3) or (cells[r, c] == 0 and num_alive == 3):
+            nxt[r, c] = 1
+            col = col_alive
 
-glfw.Terminate()
+        col = col if cells[r, c] == 1 else col_background
+        pygame.draw.rect(surface, col, (c*cellsize, r*cellsize, cellsize-1, cellsize-1))
+
+    return nxt
+
+def init(dimx, dimy):
+    cells = np.zeros((dimy, dimx))
+    pattern = np.array([[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0],
+                        [0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0],
+                        [1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                        [1,1,0,0,0,0,0,0,0,0,1,0,0,0,1,0,1,1,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]);
+    pos = (3,3)
+    cells[pos[0]:pos[0]+pattern.shape[0], pos[1]:pos[1]+pattern.shape[1]] = pattern
+
+    return cells
+
+def main(dimx, dimy, cellsize):
+    pygame.init()
+    surface = pygame.display.set_mode((dimx * cellsize, dimy * cellsize))
+    print(surface)
+    pygame.display.set_caption("John Conway's Game of Life")
+
+    cells = init(dimx, dimy)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+
+        surface.fill(col_grid)
+        cells = update(surface, cells, cellsize)
+        pygame.display.update()
+
+if __name__ == "__main__":
+    main(100, 100, 6)
--- main2.py
+++ main2.py
@@ -1,26 +1,59 @@
 import pygame
 from OpenGL.GL import *
+import numpy as np
+
 
 pygame.init()
 
+
+scale = 7
+
+dimensiony = 70
+dimensionx = 70
+
 screen = pygame.display.set_mode(
-    (800, 600),
+    (dimensionx*scale, dimensiony*scale),
     pygame.OPENGL | pygame.DOUBLEBUF
 )
 
 
 glClearColor(0.0, 1.0, 0.0, 1.0)
 
+cells = np.zeros((dimensiony, dimensionx))
+pattern = np.array([[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                    [0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0],
+                    [0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0],
+                    [1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                    [1,1,0,0,0,0,0,0,0,0,1,0,0,0,1,0,1,1,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                    [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                    [0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                    [0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]);
+
+
+def update(surface, cells, cellsize):
+    nxt = np.zeros((cells.shape[0], cells.shape[1]))
+
+    for r, c in np.ndindex(cells.shape):
+        num_alive = np.sum(cells[r-1:r+2, c-1:c+2]) - cells[r, c]
+
+        if cells[r, c] == 1 and num_alive < 2 or num_alive > 3:
+            col = col_about_to_die
+        elif (cells[r, c] == 1 and 2 <= num_alive <= 3) or (cells[r, c] == 0 and num_alive == 3):
+            nxt[r, c] = 1
+            col = col_alive
+
+        col = col if cells[r, c] == 1 else col_background
+
+    return nxt
 
 def pixel(x,y,color):
     glEnable(GL_SCISSOR_TEST)
-    glScissor(x, y, 10, 10)
+    glScissor(x*scale, y*scale, 1*scale, 1*scale)
     glClearColor(color[0],color[1],color[2],1.0)
     glClear(GL_COLOR_BUFFER_BIT)
     glDisable(GL_SCISSOR_TEST)
 
-x = 0
-speed = 1
 
 running = True
 while running:
@@ -29,13 +62,17 @@ while running:
     glClear(GL_COLOR_BUFFER_BIT)
 
     # paint
-    pixel(x, 100, (1.0,0.0,0.0))
-    x += speed
+    for y,row in enumerate(pattern):
+        for x,cell in enumerate(row):
+            if cell == 1:
+                pixel(x, y, (1.0,0.0,0.0))
 
-    if x == 800:
-        speed = -1
-    if x == 0:
-        speed = 1
+
+    #Update segun el array inicial
+    
+    #Ejecuta y muestra el nuevo array
+
+
 
     # flip
     pygame.display.flip()
